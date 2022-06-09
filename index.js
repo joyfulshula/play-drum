d04d75f49cc773f981fc6c1a8480f769974ec890
Shulammite <joyfuljohn96@gmail.com>
2022-06-09 10:22:52 +0000

    added a keyborad press incase a user trys to ply with the keyboard

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-	document.querySelectorAll(".drum")[i].addEventListener("click", butCl);
+	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 	
-	function butCl() {
+	var buttonInnerHTML = this.innerHTML;
+	makeSound(buttonInnerHTML);
 	
-	var sound = new Audio("sounds/tom-1.mp3");
-	sound.play();
- }
+	});
 }
+
+document.addEventListener("keypress", function(event){
+	makeSound(event.key);
+});
+
+function makeSound(key){
+	switch (key) {
+		case "w":
+		var crash = new Audio("sounds/crash.mp3");
+		crash.play();	
+			break;
+		case "a":
+		var kickBass = new Audio("sounds/kick-bass.mp3");
+		kickBass.play();
+			break;
+		case "s":
+		var snare = new Audio("sounds/snare.mp3");
+		snare.play();
+			break;
+		case "d":
+		var tom1 = new Audio("sounds/tom-1.mp3");
+		tom1.play();
+			break;
+		case "j":
+		var tom2 = new Audio("sounds/tom-2.mp3");
+		tom2.play();
+			break;
+		case "k":
+		var tom3 = new Audio("sounds/tom-3.mp3");
+		tom3.play();
+			break;
+		case "l":
+		var tom4 = new Audio("sounds/tom-4.mp3");
+		tom4.play();
+			break;
+		default: console.log("No sounds left");
+	}
+}
+
+
+
+// var sound = new Audio("sounds/tom-1.mp3");
+// 	sound.play();
